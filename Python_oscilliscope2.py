bcc1b4430f392192d5448d42d48d40b176b86a6a
ericsa19cnpem <eric.abbade@cnpem.br>
2024-03-27 16:54:20 -0300

    updating scope 27/03/2024

--- Python_oscilliscope2.py
+++ Python_oscilliscope2.py
@@ -1,4 +1,4 @@
-import time # std module
+import time
 import pyvisa as visa # http://github.com/hgrecco/pyvisa - pyvisa for connectivity
 import matplotlib.pyplot as plt # http://matplotlib.org/ - for plotting
 import numpy as np # http://www.numpy.org
@@ -6,14 +6,18 @@ import threading
 import datetime
 import timeit
 
-#Definindo variáveis:
-thr=0.9 #Threshold value (values above threshold will generate exceptions and will have their moment of transition from below to above threshold captured).
+#Defining variables:
+
+#General variables:
+#thr=10 #Threshold value (values above threshold will generate exceptions and will have their moment of transition from below to above threshold captured).
 ddb=0.01 #Delay to facilitate debugging.
+cycles=0 #Numbers of cycles of the code
+
+#Scale variables:
 autoset=True #True: uses autoscale. False: doesn't use autoscale (if autoset is False when scope is initialized).
 #autoset=False #True: uses autoscale. False: doesn't uses autoscale (if autoset is False when scope is initialized).
-
-autoset2=False #Ativa o autoscale a cada ciclo de medida.
-std_wfm = 125000 #standard wfm_record
+autoset2=False #Activates autoscale in each measure cicle of the loop (not only in the start).
+std_wfm = 100000 #standard wfm_record
 std_trigger = 0 #standard triger record
 std_tscale = 3.2e-08 # standard t_scale
 std_tsub =  -0.002 #standard sub-sample trigger correction
@@ -22,36 +26,88 @@ std_voff =  0 #standard reference voltage
 std_vpos = 0 #standard reference position (level)
 standard_scale=(std_wfm, std_trigger, std_tscale, std_tsub, std_vscale, std_voff, std_vpos) 
 
-#Definindo parâmetros inicias da escala do osciloscópio:
+#Trigger variables:
+threshold = 2.32 #In Volts. WILL BE ROUNDED FOR THE CLOSEST MULTIPLE OF 40 mV. Voltage level that activates trigger (example: 0.08 = 80mV). 
+trigger_type = 'EDGe' #Defines the type of the trigger: EDGe, LOGic, PULSe, BUS or VIDeo.
+trigger_channel = 'CH1' #Defines the channel of the trigger (example: CH1).
+trigger_coupling = 'DC' #Defines the coupling of the trigger: DC, High Frequency(HF) Reject, Low Frequency(LF) Reject or Noise Reject.
+trigger_slope = 'RISE' #Defines the trigger slope (rising or falling edge).
+trigger_holdoff = 20.000E-9 #In seconds. Period during which the trigger will not generate a trigger event.
 
-
-
-#unit test python
-#vunit
-#github action
-
+#Plot variables:
+y_min=1.5 #Minimal value of y-axis in plot.
+y_max=2.5 #Maximal value of y-axis in plot.
 
     
-  
+
 def read_scope():
     
-    def close(args=''):
+    def close():
         #for thread in threading.enumerate(): 
         #print("after thread started:", thread.name)
         print("start closing figure")
         time.sleep(30)
+        
         plt.close('all')
 
-    def plot_scope(scaled_time, scaled_wave):        
-        # plotting
-        plt.plot(scaled_time, scaled_wave)
-        plt.title('channel 1') #plot label
-        plt.xlabel('time (seconds)') # x label
-        plt.ylabel('voltage (volts)') # y label
-        print("look for plot window...")    
-        routine=threading.Thread(target=close, args=(1,))
-        routine.start()
+    def plot_scope(scaled_time, scaled_wave, trigger_level_read):
+        
+        print("start plot_scope")
+        min_scale_time=min(scaled_time)
+        max_scale_time=max(scaled_time)
+        #plt.plot(scaled_time, scaled_wave)
+        
+        print("trigger_level_read:", trigger_level_read)
+        fig, ax = plt.subplots()
+        ax.plot(scaled_time, scaled_wave, label='Oscilloscope Measure(V)', color='b')        
+        ax.hlines(y=float(trigger_level_read), xmin=min_scale_time, xmax=max_scale_time, label='Threshold Voltage(V)', color='r', linestyles='--')        
+        plt.legend(loc="lower right")
+        
+        
+        plt.title('Voltage of trigger activation') #plot title
+        plt.xlabel('Time (Seconds)') # x label
+        plt.ylabel('Voltage (Volts)') # y label
+        
+        
+        
+        plt.ylim(y_min, y_max) #Delimitates y-axis
+        #plt.imsave('teste.png', 'MxNx4')
+        plt.savefig('scopetrigger.png')
         plt.show()
+        time.sleep(10)
+        #cycles=cycles+1
+        
+        plt.close('all')
+        #routine=threading.Thread(target=close, daemon=True) #if not daemon, Python might raise exceptions.
+        #routine.start()
+        
+        
+
+    def horizontal_wave():
+        #Horizontal (time):
+        total_time = t_scale * wfm_record
+        t_start = (-pre_trig_record * t_scale) + t_sub
+        t_stop = t_start + total_time
+        scaled_time = np.linspace(t_start, t_stop, num=wfm_record, endpoint=False)
+        print("finished retrieve scaling factors horizontal (time)")
+        time.sleep(ddb)
+        return scaled_time
+        
+    def vertical_wave(bin_wave, wfm_record, pre_trig_record, t_scale, t_sub, v_scale, v_off, v_pos):
+        #Vertical (voltage):
+        
+        unscaled_wave = np.array(bin_wave, dtype='double') # data type conversion 
+        scaled_wave = (unscaled_wave - v_pos) * v_scale + v_off
+        print("finished retrieve scaling factors horizontal vertical (voltage)")
+        time.sleep(ddb)
+        return scaled_wave
+
+    def generate_wave(bin_wave, wfm_record, pre_trig_record, t_scale, t_sub, v_scale, v_off, v_pos, trigger_level_read):
+        #Creating scaled vectors:
+        scaled_time=horizontal_wave()
+        scaled_wave=vertical_wave(bin_wave, wfm_record, pre_trig_record, t_scale, t_sub, v_scale, v_off, v_pos)  
+        #Function plot:
+        plot_scope(scaled_time, scaled_wave, trigger_level_read)
         
     #wfm_record, pre_trig_record, t_scale, t_sub, v_scale, v_off, v_pos: 125000 0 3.2e-08 -0.002 0.08 0.0 0.0
     def writescale(wfm_record, pre_trig_record, t_scale, t_sub, v_scale, v_off, v_pos):
@@ -92,15 +148,117 @@ def read_scope():
         print("stop autoset") 
         print("autoset undone")
         time.sleep(5)
+
+    def read_trigger():
+        trigger_state_read = scope.query('TRIGger:STATE?') #Returns the state of the trigger: ARMED, AUTO, READY, SAVE or TRIGGER.
+        trigger_type_read = scope.query("TRIGger:A:TYPe?") #Returns the type of the trigger: EDGe, LOGic, PULSe, BUS or VIDeo.
+        trigger_source_read = scope.query('TRIGGER:A:EDGE:SOURCE?') #Returns the channel of the trigger (example: CH1).
+        trigger_coupling_read = scope.query('TRIGGER:A:EDGE:COUPLING?') #Returns the coupling of the trigger: DC, High Frequency(HF) Reject, Low Frequency(LF) Reject or Noise Reject.
+        trigger_slope_read = scope.query('TRIGGER:A:EDGE:SLOPE?') #Returns the trigger slope (rising or falling edge).
+        trigger_level_read = scope.query('TRIGger:A:LEVel?') #Returns the voltage level that activates trigger in Volts (example: 0.04 = 40mV)
+        trigger_holdoff_read = scope.query('TRIGGER:A:HOLDOFF:TIME?') #Returns the holdoff time (period during which the trigger will not generate a trigger event, in seconds).
+        print("trigger state:",trigger_state_read)
+        print("trigger type:",trigger_type_read)
+        print("trigger channel:",trigger_source_read)
+        print("trigger dc?:",trigger_coupling_read)
+        print("trigger border:",trigger_slope_read)  
+        print("trigger voltage (V):",trigger_level_read)
+        print("trigger holdoff time (s):",trigger_holdoff_read)
+        return(trigger_state_read, trigger_type_read, trigger_source_read, trigger_coupling_read, trigger_slope_read, trigger_level_read, trigger_holdoff_read)
+
+    def write_trigger():
+        print("start trigger writing")
+        #scope.write('TRIGger:STATE?') #Defines the state of the trigger: ARMED, AUTO, READY, SAVE or TRIGGER.
+        scope.write('TRIGger:A:TYPe '+trigger_type) #Defines the type of the trigger: EDGe, LOGic, PULSe, BUS or VIDeo.
+        print("first trigger writing succeded")
+        scope.write('TRIGGER:A:EDGE:SOURCE '+trigger_channel) #Defines the channel of the trigger (example: CH1).
+        scope.write('TRIGGER:A:EDGE:COUPLING '+trigger_coupling) #Defines the coupling of the trigger: DC, High Frequency(HF) Reject, Low Frequency(LF) Reject or Noise Reject.
+        scope.write('TRIGGER:A:EDGE:SLOPE '+trigger_slope) #Defines the trigger slope (rising or falling edge).
+        print("forth trigger writing succeded")
+        #scope.write('TRIGGER:A:LEVEL TTL') #Defines the voltage level that activates trigger in Volts (example: 0.04 = 40mV)
+        scope.write('TRIGGER:A:LEVEL '+str(threshold)) #Defines the voltage level that activates trigger in Volts (example: 0.04 = 40mV)
+        scope.write('TRIGGER:A:HOLDOFF:TIME '+str(trigger_holdoff)) #Defines the holdoff time (period during which the trigger will not generate a trigger event, in seconds).
+        print("finished trigger writing")
         
+
+    def disconnect_scope():
+        # disconnect 
+        scope.close()
+        rm.close()
+
     
+
+    def check_wave(trigger_level_read):
+        if autoset2:
+            print("start ats")
+            (wfm_record, pre_trig_record, t_scale, t_sub, v_scale, v_off, v_pos)=autoscale()
+            
+        while (scope.query('TRIGger:STATE?')!='TRIG'):              
+            print("trigger state:", scope.query('TRIGger:STATE?'))           
+            time.sleep(0.005)
+
+        print("trigger state:", scope.query('TRIGger:STATE?'))
+        time.sleep(0.5)
+
+        # acquisition
+        scope.write('acquire:state OFF') # stop 
+        print("try will start")
+        scope.write('acquire:stopafter SEQUENCE;state ON') # single
+        try:
+            r = scope.query('*opc?')
+        except:
+            print("except")
+            scope.write('AUTOSET EXECute') #autoscale.
+            time.sleep(ddb)
+            r = scope.query('*opc?')
+
+        print("finished acquisition")
+        time.sleep(ddb)
+
+        # curve configuration
+        max_wave=0
+        scope.write('data:encdg SRIBINARY') # signed integer
+        scope.write('data:source CH1')
+        scope.write('data:start 1')
+        acq_record = int(scope.query('horizontal:recordlength?'))
+        scope.write('data:stop {}'.format(acq_record))
+        scope.write('wfmoutpre:byt_n 1') # 1 byte per sample
+        print("finished curve configuration")
+        time.sleep(ddb)
+              
+        bin_wave=scope.query_binary_values('curve?', datatype='b', container=np.array, chunk_size = 1024**2)
+        end_time = datetime.datetime.now()
+        print("bin_wave:", bin_wave)
+        print("max_wave:", max(bin_wave))
+        (wfm_record, pre_trig_record, t_scale, t_sub, v_scale, v_off, v_pos)=readscale()
+        max_wave=(max(bin_wave) - v_pos) * v_scale + v_off
+        print(wfm_record, pre_trig_record, t_scale, t_sub, v_scale, v_off, v_pos)
+        print("len(bin_wave):", len(bin_wave))
+        print("max_wave:", max_wave)        
+        time.sleep(1) #small delays makes communication too fast, which will cause troubles in restart communication when scripts restarts.     
+       
+
+
+            
+        """
+        data = ['Brazil', 652090, 'BR', 'BRA']
+        with open('scope.csv', 'w', encoding='UTF8') as f:
+            writer = csv.writer(f)            
+            #writer.writerow(bin_wave)
+            writer.writerow(data) 
+        """
+        time.sleep(ddb)
+        disconnect_scope()
+        generate_wave(bin_wave, wfm_record, pre_trig_record, t_scale, t_sub, v_scale, v_off, v_pos, trigger_level_read)
     
     # VISA descriptor to identify the test and measurement device
     # Please update the VISA descriptor from the query result from pyvisa visa_address = 'USB0::1689::261::Q300209::0::INSTR'
     visa_address='USB0::0x0699::0x0378::C010837::0::INSTR'
     rm = visa.ResourceManager()
     scope = rm.open_resource(visa_address)
-    print(scope.query('*IDN?'))   
+    print(scope.query('*IDN?'))
+
+    
     scope.timeout = 10000 # ms
     scope.encoding = 'latin_1'    
     scope.read_termination = '\n'   
@@ -120,106 +278,44 @@ def read_scope():
         time.sleep(2)
         (wfm_record, pre_trig_record, t_scale, t_sub, v_scale, v_off, v_pos)=readscale()
         
-    
-    max_wave=0
-    # data query
-    while (max_wave<thr):
-        
-        if autoset2:
-            print("start ats")
-            (wfm_record, pre_trig_record, t_scale, t_sub, v_scale, v_off, v_pos)=autoscale()
-        
-        # acquisition
-        scope.write('acquire:state OFF') # stop 
-        print("try will start")
-        scope.write('acquire:stopafter SEQUENCE;state ON') # single
-        try:
-            r = scope.query('*opc?')
-        except:
-            print("except")
-            scope.write('AUTOSET EXECute') #autoscale.
-            time.sleep(ddb*1000)
-        
-        print("finished acquisition")
-        time.sleep(ddb)
-        
-        # curve configuration
-        scope.write('data:encdg SRIBINARY') # signed integer
-        scope.write('data:source CH1')
-        scope.write('data:start 1')
-        acq_record = int(scope.query('horizontal:recordlength?'))
-        scope.write('data:stop {}'.format(acq_record))
-        scope.write('wfmoutpre:byt_n 1') # 1 byte per sample
-        print("finished curve configuration")
-        time.sleep(ddb)
-        start_time = datetime.datetime.now()
-        bin_wave=scope.query_binary_values('curve?', datatype='b', container=np.array, chunk_size = 1024**2)
-        end_time = datetime.datetime.now()
-        max_wave=(max(bin_wave) - v_pos) * v_scale + v_off
-        print(wfm_record, pre_trig_record, t_scale, t_sub, v_scale, v_off, v_pos)
-        print("finished data query")    
-        start_time2 = datetime.datetime.now()
-        print("len(bin_wave):", len(bin_wave))
-        end_time2 = datetime.datetime.now()
-        print("max_wave:", max_wave)        
-        time.sleep(10) #small delays makes communication too fast, which will cause troubles in restart communication when scripts restarts.   
-        execution_time = end_time - start_time
-        execution_time2 = end_time2 - start_time2
-        print("Time taken to execute the commands:", execution_time, "second command", execution_time2)
-        
-    """
-    data = ['Brazil', 652090, 'BR', 'BRA']
-    with open('scope.csv', 'w', encoding='UTF8') as f:
-        writer = csv.writer(f)            
-        #writer.writerow(bin_wave)
-        writer.writerow(data) 
-    """
-    time.sleep(ddb)
+    (trigger_state_read, trigger_type_read, trigger_source_read, trigger_coupling_read, trigger_slope_read, trigger_level_read, trigger_holdoff_read)=read_trigger()
+    write_trigger()
+    (trigger_state_read, trigger_type_read, trigger_source_read, trigger_coupling_read, trigger_slope_read, trigger_level_read, trigger_holdoff_read)=read_trigger()
+    time.sleep(1)
+    time.sleep(5)
+    check_wave(trigger_level_read)
 
     
-    # disconnect
-    scope.close()
-    rm.close()
-    # create scaled vectors
-    # horizontal (time)
+        
 
-    total_time = t_scale * wfm_record
-    t_start = (-pre_trig_record * t_scale) + t_sub
-    t_stop = t_start + total_time
-    scaled_time = np.linspace(t_start, t_stop, num=wfm_record, endpoint=False)
-    print("finished retrieve scaling factors")
-    time.sleep(ddb)
-
-    # vertical (voltage)
-    unscaled_wave = np.array(bin_wave, dtype='double') # data type conversion 
-    scaled_wave = (unscaled_wave - v_pos) * v_scale + v_off
-    print("finished vertical (voltage)")
-    time.sleep(ddb)
-
-    #Plot da função:
-    plot_scope(scaled_time, scaled_wave)
+       
    
      
 error=0
+read_scope()
 while (error<10):
     try:
         print("activate read_scope()")
         read_scope()
         error=0
     except:
+        time.sleep(10)
         error=error+1
         print("Error in communication with scope number:", error, "(application will close after 10 errors in a row).")
     time.sleep(2)
+scope.close()
+rm.close()
 print("Application finished due to errors in communication with scope")
 time.sleep(4)
  
 """
 #Remains:
+Trigger catches EVERY DC transitions AND NEVER AFTER THEN.
+Erros that sometimes occurs in plots.
 Check to see if it´s possible to use a defined scale when autoscale was used after initialization in a way that the scales are applieds and doesn´t generates error after several runs.
 Real time measure.
 Correct problem to restart communication after deafult scale.
 Correct voltage with different scale
-Correct error DC;
 correct fail when amplituded changed;
 automatic autoscale to avoid signal lost 9already happened due to it;
 create function to identify voltage value;
@@ -227,3 +323,9 @@ save file to csv
 add try execept tp waveform generator script.
 """
 #https://www.tek.com/en/documents/application-note/using-raspberry-pi-to-control-your-oscilloscope
+
+#unit test python
+#vunit
+#github action
+
+
